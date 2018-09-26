c915434980708ac1e565d59ef9cb4b131decab9a
DreneB <deanarene1@gmail.com>
2018-09-26 16:36:52 -0700

    working scanner

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Scanner from './Scanner';
+import Result from './Result';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+export default class App extends React.Component{
+  
+    constructor() {
+      super();
 
-export default App;
+      this.state = {
+        scanning: false,
+        results: []
+      }
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this._scan}>{this.state.scanning ? 'Stop' : 'Start'}</button>
+                <ul className="results">
+                    {this.state.results.map((result) => (<Result key={result.codeResult.code} result={result} />))}
+                </ul>
+                {this.state.scanning ? <Scanner onDetected={this._onDetected}/> : null}
+            </div>
+        );
+    }
+
+    _scan = () => {
+        this.setState({scanning: !this.state.scanning});
+    }
+
+    _onDetected = (result) => {
+
+        if(this.state.results.length < 5) {
+          this.setState
+          console.log('RESULT:', result);
+          this.setState({results: this.state.results.concat([result])});
+
+        }
+        
+        
+    }
+};
---
+++ src/Result.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Result extends React.Component{
+
+    render() {
+        const result = this.props.result;
+
+        if (!result) {
+            return null;
+        }
+        return (
+            <li>
+            {result.codeResult.code} [{result.codeResult.format}]
+            </li>
+        );
+    }
+}
+
+Result.propTypes = {
+    result: PropTypes.object
+}
+
+export default Result;
