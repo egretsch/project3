779f4e013553b13149d04bfa4ecf3079726a37b8
Legend <jasonrfelipe@gmail.com>
2018-09-27 19:53:07 -0700

    added yarn

--- src/Scanner.js
+++ src/Scanner.js
@@ -27,17 +27,18 @@ class Scanner extends React.Component {
             numOfWorkers: 2,
             decoder: {
                 readers : [ 
-                "code_128_reader",
+                // "code_128_reader",
                 'ean_reader',
-                'ean_8_reader',
-                'code_39_reader',
-                'code_39_vin_reader',
-                'codabar_reader',
-                'upc_reader',
-                'upc_e_reader',
-                'i2of5_reader',
-                '2of5_reader',
-                'code_93_reader']
+                // 'ean_8_reader',
+                // 'code_39_reader',
+                // 'code_39_vin_reader',
+                // 'codabar_reader',
+                // 'upc_reader',
+                // 'upc_e_reader',
+                // 'i2of5_reader',
+                // '2of5_reader',
+                // 'code_93_reader'
+            ]
             },
             locate: true
         }, function(err) {
