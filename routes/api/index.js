c261ed562ec63b657fea06ebff39b607540b096c
Edward <edwardgretsch@gmail.com>
2018-10-05 18:19:04 -0700

    update103

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
-const productRoutes = require('./products');
+//const productRoutes = require('./products');
 const ingredientRoutes = require('./ingredients');
 const userRoutes = require("./user");
 
 // routes
-router.use('/products', productRoutes);
+//router.use('/products', productRoutes);
 router.use('/ingredients', ingredientRoutes);
 // User routes
 router.use("/user", userRoutes);
